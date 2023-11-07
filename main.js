ff7560ddeddaeda8d84ca0c0da9fcee447a03d1a
antoniomorais91 <antonio.vinicius.morais@gmail.com>
2023-11-06 23:04:23 -0300

    Primeiro Upload

---
+++ main.js
@@ -0,0 +1,7 @@
+/* Primeira Forma */
+
+const oneStars = 2, twoStars = 15,treeStars = 18, fourStars = 25, fiveStars = 40;
+
+let media = ((oneStars * 1) + (twoStars * 2) + (treeStars * 3) + (fourStars * 4) + (fiveStars * 5)) / (oneStars + twoStars + treeStars + fourStars + fiveStars);
+
+console.log("O resultado da média foi de " + media + " estrelas.");
